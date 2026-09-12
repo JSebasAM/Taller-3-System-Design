460b13bc85301b034aa115ff4ad5425838910210
stevendz2002 <jhon.loaiza01@uceva.edu.co>
2026-09-12 00:06:46 -0500

    feat: se exportan ImageAtom y DotIndicatorAtom en public-api

--- projects/design-system-bootstrap/src/public-api.ts
+++ projects/design-system-bootstrap/src/public-api.ts
@@ -10,6 +10,8 @@ export { IconAtom } from './lib/presentation/atoms/icon/icon.atom';
 export { BadgeAtom } from './lib/presentation/atoms/badge/badge.atom';
 export { ButtonAtom } from './lib/presentation/atoms/button/button.atom';
 export { ContainerAtom } from './lib/presentation/atoms/container/container-atom';
+export { ImageAtom } from './lib/presentation/atoms/image/image.atom';
+export { DotIndicatorAtom } from './lib/presentation/atoms/dot-indicator/dot-indicator.atom';
 
 //Molecules
 export { NavLinkMolecule } from './lib/presentation/molecules/nav-link/nav-link.molecule';
